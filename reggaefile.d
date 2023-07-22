59031ecee37a6c8a90c574b55800c824bafbea36
John Colvin <john.loughran.colvin@gmail.com>
2023-07-22 15:20:52 +0100

    fix build

--- reggaefile.d
+++ reggaefile.d
@@ -1,9 +1,28 @@
 import reggae;
 
-alias test = executable!(
-    ExeName("test"),
-    Sources!(["src"]),
-    Flags("-g -unittest -main -i"),
-    ImportPaths(["../src", "src"])
+enum unitThreadedSourceDirs = [
+        "unit-threaded/subpackages/assertions/source",
+        "unit-threaded/subpackages/exception/source",
+        "unit-threaded/subpackages/from/source",
+        "unit-threaded/subpackages/runner/source",
+];
+
+alias unit_threaded = staticLibrary!(
+    "unit-threaded.a",
+    Sources!(unitThreadedSourceDirs),
+    Flags(),
+    ImportPaths(unitThreadedSourceDirs)
 );
-mixin build!test;
+
+alias testObjs = objectFiles!(
+    Sources!(["src", "SDC/src/source"], Files(["tests/all.d"])),
+    Flags("-g -unittest"),
+    ImportPaths(["src", "SDC/src"] ~ unitThreadedSourceDirs)
+);
+
+alias test = link!(
+    ExeName("test"),
+    () => testObjs ~ unit_threaded
+);
+
+mixin build!(test, unit_threaded);
