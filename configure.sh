a98f90718bbd27f8d481e871d0dd6557c7748277
John Colvin <john.loughran.colvin@gmail.com>
2023-07-22 15:25:45 +0100

    add configure.sh

---
+++ configure.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+D_COMPILER=ldc-1.33.0-beta2
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd $SCRIPT_DIR
+mkdir -p dlang
+pushd dlang
+[ -f ./install.sh ] || curl https://dlang.org/install.sh --output ./install.sh
+bash ./install.sh $D_COMPILER --path .
+ln -sf $D_COMPILER ldc
+popd
+
+./dlang/ldc/bin/reggae --dc `pwd`/dlang/ldc/bin/ldmd2
